c69dd10dd11922abeb0eadd45266a1d0cc83dc7b
ArianaRuggiero <aer24@njit.edu>
2020-02-25 18:04:38 -0500

    Added Calculator file

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+class Calculator:
+        def add(self, num1, num2):
+                return num1 + num2
+        def subtract(self, num1, num2):
+                return num1 - num2
+        def multiply(self, num1, num2):
+                return num1 * num2
+        def divide(self, num1, num2):
+                return num 1 / num2
+if __name__ == "__main__":
+        c = Calculator()
+        Print (c.add(1,2))
+class calculator:
+def test_calc_add():
+        	from Calculator import Calculator
+        	c = Calculator()
+        	assert c.add(2,2) == 4
+class calculator:
+	def test_calc_divide():
+        	from Calculator import Calculator
+        	c = Calculator()
+        	assert c.divide(6,2) == 3
+def test_calc_multiply():
+        from Calculator import Calculator
+        c = Calculator()
+        assert c.multiply(2,2) == 4
+class Calculator: 
+def test_calc_subtract():
+        from Calculator import Calculator
+        c = Calculator()
+        assert c.subtract(10,5) == 5
+def test_calc_subtract_fail():
+        from Calculator import Calculator
+        c = Calculator()
+        assert c.subtract(10,5) != 15
+def test_calc_subtract():
+        from Calculator import Calculator
+        c = Calculator()
+        Assert c.subtract(10,5) == 5
+def test_calc_subtract_fail():
+        from Calculator import Calculator
+        c = Calculator()
+        assert c.subtract(10,5) != 15
